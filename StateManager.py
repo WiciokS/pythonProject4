719cc697c003ae9a72ff31e4b0f324daeaedacde
LastWeekNextDay <doesn@mail.ru>
2023-11-30 02:04:20 +0200

    created StateFactory

--- StateManager.py
+++ StateManager.py
@@ -1,3 +1,19 @@
+from GameplayState import GameplayState, UnpausedGameplayState, PausedGameplayState
+from State import StateName
+
+
+class StateFactory:
+    @staticmethod
+    def create_state(state_name, context):
+        if state_name == StateName.GAMEPLAY:
+            return GameplayState(context)
+        elif state_name == StateName.UNPAUSED_GAMEPLAY:
+            return UnpausedGameplayState(context)
+        elif state_name == StateName.PAUSED_GAMEPLAY:
+            return PausedGameplayState(context)
+        else:
+            raise ValueError("Invalid state name")
+
 
 class StateManager:
     def __init__(self, default_state):
