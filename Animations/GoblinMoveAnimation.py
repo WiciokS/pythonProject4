f4300dd285eb2e5baf77a25e0760da4fd2357881
LastWeekNextDay <doesn@mail.ru>
2023-12-01 18:22:39 +0200

    fixed animations missing last frame

--- Animations/GoblinMoveAnimation.py
+++ Animations/GoblinMoveAnimation.py
@@ -6,7 +6,7 @@ from Animations.Base.Animation import Animation
 class GoblinMoveAnimation(Animation):
     def __init__(self):
         frames = []
-        for i in range(1, 4):
+        for i in range(1, 5):
             frames.append(pygame.image.load(f"Sprites/Goblin/MoveAnim/Frame{i}.png").convert_alpha())
 
         super().__init__(frames, 30, True)
--- Animations/OrcMoveAnimation.py
+++ Animations/OrcMoveAnimation.py
@@ -6,7 +6,7 @@ from Animations.Base.Animation import Animation
 class OrcMoveAnimation(Animation):
     def __init__(self):
         frames = []
-        for i in range(1, 4):
+        for i in range(1, 5):
             frames.append(pygame.image.load(f"Sprites/Orc/MoveAnim/Frame{i}.png").convert_alpha())
 
         super().__init__(frames, 30, True)
